a6f293fc95cad0fa441a89099582cb0f3784f693
Александр <cany245@mail.ru>
2022-12-01 19:44:42 +0300

    Hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static int addTwo(int a, int b) {
+        return a + b + 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(addTwo(5, 5));
+    }
+}
---
+++ src/test/java/TestApp.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestApp {
+    @Test
+    public void test() {
+        assertEquals(12, Main.addTwo(5, 5));
+    }
+}
